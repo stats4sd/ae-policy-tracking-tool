cde78c24d8f2e7455fa467141ae329820db83d00
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2023-11-28 14:05:26 +0000

    adds seeder for types

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AePrincipleRecommendationTableSeeder::class);
         $this->call(PriorityActionsTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
+        $this->call(TypesTableSeeder::class);
     }
 }
---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TypesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('types')->delete();
+        
+        \DB::table('types')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Status',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Measures creating perverse incentives',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Measures that go beyond policy recommendations',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'View from a Civil Society perspective',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+        ));
+        
+        
+    }
+}
