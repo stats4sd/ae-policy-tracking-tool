0784749a22a1a96f38b77761e1cb9f9903169d6b
Dave Mills <d.e.mills@stats4sd.org>
2024-08-05 17:04:48 +0100

    redirect to policy list after add/edit

--- app/Filament/App/Resources/PolicyResource/Pages/CreatePolicy.php
+++ app/Filament/App/Resources/PolicyResource/Pages/CreatePolicy.php
@@ -12,4 +12,10 @@ class CreatePolicy extends CreateRecord
 
     protected ?string $heading = 'Upload / Link to Policy Document(s)';
     protected ?string $subheading = 'Please upload the policy document(s) or provide the URL to the document(s) that are part of the assessment.';
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
 }
