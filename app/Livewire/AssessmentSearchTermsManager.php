5c354e99188fd084de1057fb5add1905dcd68839
Dave Mills <d.e.mills@stats4sd.org>
2025-12-04 16:12:02 +0000

    WIP

---
+++ app/Livewire/AssessmentSearchTermsManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AssessmentSearchTermsManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.assessment-search-terms-manager');
+    }
+}
