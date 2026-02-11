5b569400a629fa58406a64353863a6a9c0f10b78
Dave Mills <d.e.mills@stats4sd.org>
2026-02-11 14:36:48 +0000

    add custom admin dashboard at root of admin panel

---
+++ app/Filament/Admin/Pages/AdminDashboard.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Admin\Pages;
+
+use BackedEnum;
+use Filament\Pages\Page;
+use Filament\Panel;
+use Filament\Support\Icons\Heroicon;
+
+class AdminDashboard extends Page
+{
+    protected static string $routePath = '/';
+
+    protected static string | BackedEnum | null $navigationIcon = Heroicon::Cog6Tooth;
+
+    public static function getRoutePath(Panel $panel): string
+    {
+        return static::$routePath;
+    }
+
+    protected string $view = 'filament.admin.pages.admin-dashboard';
+}
