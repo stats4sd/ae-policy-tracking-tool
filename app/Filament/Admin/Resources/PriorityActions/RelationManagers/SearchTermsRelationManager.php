a8e415f4c4bec00bc95b57d894c270c831ecd2bf
Dave Mills <d.e.mills@stats4sd.org>
2026-03-09 17:17:39 +0000

    add Stichoza translation package

--- app/Filament/Admin/Resources/PriorityActions/RelationManagers/SearchTermsRelationManager.php
+++ app/Filament/Admin/Resources/PriorityActions/RelationManagers/SearchTermsRelationManager.php
@@ -2,17 +2,23 @@
 
 namespace App\Filament\Admin\Resources\PriorityActions\RelationManagers;
 
+use App\Jobs\TranslateSearchTerm;
 use App\Models\Language;
+use App\Models\SearchTerm;
+use Filament\Actions\Action;
+use Filament\Actions\BulkAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Forms;
+use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Collection;
 
 class SearchTermsRelationManager extends RelationManager
 {
@@ -58,11 +64,40 @@ class SearchTermsRelationManager extends RelationManager
                 CreateAction::make(),
             ])
             ->recordActions([
+                Action::make('auto_translate')
+                    ->label('Auto Translate')
+                    ->icon('heroicon-o-language')
+                    ->requiresConfirmation()
+                    ->modalDescription('This will automatically fill in any missing translations using the English phrase and Google Translate. Existing translations will not be overwritten.')
+                    ->action(function (SearchTerm $record): void {
+                        dispatch_sync(new TranslateSearchTerm($record));
+
+                        Notification::make()
+                            ->title('Translations updated')
+                            ->success()
+                            ->send();
+                    }),
                 EditAction::make(),
                 DeleteAction::make(),
             ])
             ->groupedBulkActions([
                 BulkActionGroup::make([
+                    BulkAction::make('auto_translate_selected')
+                        ->label('Auto Translate')
+                        ->icon('heroicon-o-language')
+                        ->requiresConfirmation()
+                        ->modalDescription('This will automatically fill in any missing translations for the selected search terms using the English phrase and Google Translate. Existing translations will not be overwritten.')
+                        ->action(function (Collection $records): void {
+                            foreach ($records as $record) {
+                                dispatch_sync(new TranslateSearchTerm($record));
+                            }
+
+                            Notification::make()
+                                ->title('Translations updated')
+                                ->success()
+                                ->send();
+                        })
+                        ->deselectRecordsAfterCompletion(),
                     DeleteBulkAction::make(),
                 ]),
             ]);
--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google_translate' => [
+        'key' => env('GOOGLE_TRANSLATE_API_KEY'),
+    ],
+
 ];
