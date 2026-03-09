b8dc684025cb6dd4bb93bb3e2b5be0f3f01de200
Dave Mills <d.e.mills@stats4sd.org>
2026-03-09 17:24:25 +0000

    let assessment teams edit search terms in multi languages

--- app/Filament/App/Clusters/Setup/Resources/SearchTerms/Tables/SearchTermsTable.php
+++ app/Filament/App/Clusters/Setup/Resources/SearchTerms/Tables/SearchTermsTable.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\App\Clusters\Setup\Resources\SearchTerms\Tables;
 
+use App\Models\SearchTerm;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Facades\Filament;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Filters\SelectFilter;
@@ -19,6 +21,20 @@ class SearchTermsTable
                 TextColumn::make('priorityAction.code_and_short_name')
                     ->searchable(),
                 TextColumn::make('phrase')
+                    ->label(function (): string {
+                        $language = Filament::getTenant()->language;
+
+                        return $language
+                            ? 'Phrase ('.$language->getTranslation('name', 'en').')'
+                            : 'Phrase';
+                    })
+                    ->getStateUsing(function (SearchTerm $record): string {
+                        $languageId = Filament::getTenant()->language_id;
+
+                        return $languageId
+                            ? $record->getTranslation('phrase', $languageId, useFallbackLocale: true)
+                            : $record->phrase;
+                    })
                     ->searchable(),
             ])
             ->filtersLayout(FiltersLayout::AboveContent)
