4e4ab62afc734b1e2127b5dce24a31fdc61f52f6
Dave Mills <d.e.mills@stats4sd.org>
2026-04-17 13:28:10 +0100

    fix npm audit issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,6 @@ class User extends \Stats4sd\FilamentTeamManagement\Models\User
     /** @return BelongsToMany<Assessment, $this> */
     public function assessments(): BelongsToMany
     {
-        return $this->belongsToMany(Assessment::class, 'assessment_user', 'user_id', 'assessment_id');
+        return $this->teams();
     }
 }
